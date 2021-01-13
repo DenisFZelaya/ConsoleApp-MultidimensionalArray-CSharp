628dd696b0219525bcf9eb7777d9f82d4f8d7bb3
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-01-13 13:37:39 -0600

    Initial commit

---
+++ ConsoleApp-MultidimensionalArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp_MultidimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Multidimensional Array
+            int[,] arr = new int[3, 3]; // declaracion de array 2d
+            int[,,] arr2 = new int[3, 3, 3]; // declaracion de array 3d
+        }
+    }
+}
